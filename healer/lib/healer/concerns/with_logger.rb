df7efafcec250cd2531ffd8e2add776868804a8f
Matiss Gaigals <matiss.gaigals@workday.com>
2025-03-22 20:16:21 +0200

    Move logger from concern to service class

--- healer/lib/healer/concerns/with_logger.rb
+++
@@ -1,9 +0,0 @@
-module Healer::Concerns::WithLogger
-  extend ActiveSupport::Concern
-
-  private
-
-  def log(error_event_id, action_name, result)
-    ::Healer::ActivityLog.create!(error_event_id: error_event_id, action_name: action_name, result: result)
-  end
-end
--- healer/lib/healer/dynamic_method/create.rb
+++ healer/lib/healer/dynamic_method/create.rb
@@ -1,6 +1,4 @@
 class Healer::DynamicMethod::Create
-  include ::Healer::Concerns::WithLogger
-
   attr_reader :klass, :action_name, :error
 
   def initialize(klass:, action_name:, error:)
@@ -28,9 +26,13 @@ class Healer::DynamicMethod::Create
       ::Healer::ErrorEvent.create!(class_name: klass.name, method_name: action_name, error: error.to_json)
   end
 
+  def log(action_name, result)
+    healer_error_event.activity_logs.create!(action_name: action_name, result: result)
+  end
+
   def openai_prompt
     prompt = ::Healer::Openai::Prompt.call(klass: klass, action_name: action_name, error: error)
-    log(healer_error_event.id, "AI prompt", prompt.to_json)
+    log("AI prompt", prompt.to_json)
     healer_error_event.update!(prompt: prompt.to_json)
     prompt
   end
@@ -38,7 +40,7 @@ class Healer::DynamicMethod::Create
   def openai_response
     @openai_response ||= begin
       response = ::Healer::Openai::Response.call(prompt: openai_prompt)
-      log(healer_error_event.id, "AI response", response.to_json)
+      log("AI response", response.to_json)
       healer_error_event.update!(response: response.to_json, method_source: response["method_source"])
       response
     end
@@ -57,13 +59,11 @@ class Healer::DynamicMethod::Create
   def update_healer_error_event_result(result)
     if result == false
       log(
-        healer_error_event.id,
         "Dynamic method test fail",
         "Healer::ErrorEvent ID=#{healer_error_event.id} not mitigated".to_json
       )
     else
       log(
-        healer_error_event.id,
         "Dynamic method test success",
         "Healer::ErrorEvent ID=#{healer_error_event.id} mitigated".to_json
       )
